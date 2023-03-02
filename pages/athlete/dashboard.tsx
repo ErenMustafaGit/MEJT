9b06856a7113df8f4c5b9a4680f12ceca224afab
Eren Mustafa Ceylan <eren-mustafa.ceylan@insa-lyon.fr>
2023-03-02 09:24:30 +0100

    Resolved import and duplicate data

--- pages/athlete/dashboard.tsx
+++ pages/athlete/dashboard.tsx
@@ -1,10 +1,17 @@
 import Layout from "@/components/layout";
 import DataGrid from "@/components/home/data-grid";
-import Link from "next/link";
 
 import Balancer from "react-wrap-balancer";
 import { motion } from "framer-motion";
-import { FADE_DOWN_ANIMATION_VARIANTS, BLUE_FILL_GRAPH, BLUE_LINE_GRAPH, FADE_DOWN_ANIMATION_VARIANTS, ORANGE_FILL_GRAPH, ORANGE_LINE_GRAPH, VIOLET_FILL_GRAPH, VIOLET_LINE_GRAPH } from "@/lib/constants";
+import {
+  BLUE_FILL_GRAPH,
+  BLUE_LINE_GRAPH,
+  FADE_DOWN_ANIMATION_VARIANTS,
+  ORANGE_FILL_GRAPH,
+  ORANGE_LINE_GRAPH,
+  VIOLET_FILL_GRAPH,
+  VIOLET_LINE_GRAPH,
+} from "@/lib/constants";
 import SessionData from "models/session-data";
 import { useRouter } from "next/router";
 import Graphic from "@/components/graphics/graphic";
@@ -19,7 +26,7 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js';
+} from "chart.js";
 
 ChartJS.register(
   CategoryScale,
@@ -28,16 +35,14 @@ ChartJS.register(
   LineElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
 );
 
-
-
-
-export default function Login() {
+export default function Dashboard() {
   const router = useRouter();
   const API_URL = process.env.MEJT_API_URL;
 
+  // TO BE DELETED (DATA REPLACING API CALL)
   const sessions: SessionData[] = Array(6).fill({
     id: "1",
     name: "Entrainement : Bas du corps	1",
@@ -52,60 +57,60 @@ export default function Login() {
     location: "Bat Ava Lovelace",
     feedback: false,
   });
-  
-  const sessions = [
-  {
-    teamName: "l'équipe du dimanche l'équipe du dimanche l'équipe du dimanche",
-    sessionId: 20,
-    date: "2012-04-28T18:25:43.511Z",
-    place: "le stade de sport, 69100 Villeurbanne",
-    description: "entrainement du bas du corps en vu de la compétition",
-    name: "entrainement pre-compet",
-  },
-  {
-    teamName: "les ours",
-    sessionId: 30,
-    date: "2012-06-27T18:25:43.511Z",
-    place: "la salle de sport, 69000 Lyon",
-    description: "entrainement du haut du corps",
-    name: "entrainement post-vacances",
-  },
-];
 
-  const config = 
-  {
-    title:"Stress",
-    xValues:[
-      Date.parse("2021-01-20"), 
+  const sessionsLess = [
+    {
+      teamName:
+        "l'équipe du dimanche l'équipe du dimanche l'équipe du dimanche",
+      sessionId: 20,
+      date: "2012-04-28T18:25:43.511Z",
+      place: "le stade de sport, 69100 Villeurbanne",
+      description: "entrainement du bas du corps en vu de la compétition",
+      name: "entrainement pre-compet",
+    },
+    {
+      teamName: "les ours",
+      sessionId: 30,
+      date: "2012-06-27T18:25:43.511Z",
+      place: "la salle de sport, 69000 Lyon",
+      description: "entrainement du haut du corps",
+      name: "entrainement post-vacances",
+    },
+  ];
+
+  const config = {
+    title: "Stress",
+    xValues: [
+      Date.parse("2021-01-20"),
       Date.parse("2022-01-20"),
-       Date.parse("2022-07-14"), 
-       Date.parse("2022-11-15"), 
-       Date.parse("2022-12-12"),
-       Date.parse("2023-02-05"),
-       Date.parse("2023-02-15"),
-       Date.parse("2023-02-17"),
-       Date.parse("2023-02-25"),
-       Date.parse("2023-02-27"),
-       Date.parse("2023-02-28"),
-       Date.now()
-      ],
-    yValues:[0,10,5,8,2,3,0,0,2,9,5,6],
-    lineColor:BLUE_LINE_GRAPH,
-    fillColor:BLUE_FILL_GRAPH
+      Date.parse("2022-07-14"),
+      Date.parse("2022-11-15"),
+      Date.parse("2022-12-12"),
+      Date.parse("2023-02-05"),
+      Date.parse("2023-02-15"),
+      Date.parse("2023-02-17"),
+      Date.parse("2023-02-25"),
+      Date.parse("2023-02-27"),
+      Date.parse("2023-02-28"),
+      Date.now(),
+    ],
+    yValues: [0, 10, 5, 8, 2, 3, 0, 0, 2, 9, 5, 6],
+    lineColor: BLUE_LINE_GRAPH,
+    fillColor: BLUE_FILL_GRAPH,
   };
 
-  const config2 ={
+  const config2 = {
     ...config,
-    title:'Tiredness',
-    lineColor:ORANGE_LINE_GRAPH,
-    fillColor:ORANGE_FILL_GRAPH
+    title: "Tiredness",
+    lineColor: ORANGE_LINE_GRAPH,
+    fillColor: ORANGE_FILL_GRAPH,
   };
 
   const config3 = {
     ...config,
-    title:'Fitness',
-    lineColor:VIOLET_LINE_GRAPH,
-    fillColor:VIOLET_FILL_GRAPH
+    title: "Fitness",
+    lineColor: VIOLET_LINE_GRAPH,
+    fillColor: VIOLET_FILL_GRAPH,
   };
 
   return (
@@ -134,7 +139,7 @@ export default function Login() {
               <Balancer>Feedback less sessions</Balancer>
             </h2>
             <div className="m-6 flex flex-wrap gap-6">
-              {sessions.map((session, key) => (
+              {sessionsLess.map((session, key) => (
                 <SessionCard
                   key={key}
                   id={session.sessionId}
@@ -151,34 +156,34 @@ export default function Login() {
             <h2 className="text-3xl font-bold text-rblue-700">
               <Balancer>Teams</Balancer>
             </h2>
-            <div className="flex flex-col lg:flex-row w-full h-auto lg:h-2/4 mt-5">
-              <div className="w-full lg:w-1/3 h-auto lg:h-2/4">
-              <Graphic
-                title={config.title}
-                xValues={config.xValues}
-                yValues={config.yValues}
-                lineColor={config.lineColor}
-                fillColor={config.fillColor}
-               />
-              </div>
-
-              <div className="w-full lg:w-1/3 h-auto lg:h-2/4">
+            <div className="mt-5 flex h-auto w-full flex-col lg:h-2/4 lg:flex-row">
+              <div className="h-auto w-full lg:h-2/4 lg:w-1/3">
                 <Graphic
-                title={config2.title}
-                xValues={config2.xValues}
-                yValues={config2.yValues}
-                lineColor={config2.lineColor}
-                fillColor={config2.fillColor}
+                  title={config.title}
+                  xValues={config.xValues}
+                  yValues={config.yValues}
+                  lineColor={config.lineColor}
+                  fillColor={config.fillColor}
                 />
               </div>
 
-              <div className="w-full lg:w-1/3 h-auto lg:h-2/4">
+              <div className="h-auto w-full lg:h-2/4 lg:w-1/3">
                 <Graphic
-                title={config3.title}
-                xValues={config3.xValues}
-                yValues={config3.yValues}
-                lineColor={config3.lineColor}
-                fillColor={config3.fillColor}
+                  title={config2.title}
+                  xValues={config2.xValues}
+                  yValues={config2.yValues}
+                  lineColor={config2.lineColor}
+                  fillColor={config2.fillColor}
+                />
+              </div>
+
+              <div className="h-auto w-full lg:h-2/4 lg:w-1/3">
+                <Graphic
+                  title={config3.title}
+                  xValues={config3.xValues}
+                  yValues={config3.yValues}
+                  lineColor={config3.lineColor}
+                  fillColor={config3.fillColor}
                 />
               </div>
             </div>
@@ -187,7 +192,6 @@ export default function Login() {
             <h2 className="text-3xl font-bold text-rblue-700">
               <Balancer>Sessions</Balancer>
             </h2>
-
 
             <div className="flex w-full flex-col">
               <DataGrid
