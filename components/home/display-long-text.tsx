ce0b08501be1d66a2af698bc9096a7e249ef4cdb
Eren Mustafa Ceylan <eren-mustafa.ceylan@insa-lyon.fr>
2023-03-03 09:22:42 +0100

    Feedback session for athletes created; icon assets added

---
+++ components/home/display-long-text.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Balancer from "react-wrap-balancer";
+
+export default function DisplayLongText({
+  text,
+  title,
+}: {
+  text: string;
+  title: string;
+}) {
+  return (
+    <div className="flex w-full flex-col gap-2">
+      <p className="px-1 text-xs font-semibold">{title}</p>
+      <div className="w-full rounded-lg border border-gray-500/30 p-2">
+        <p className="text-gray-500">{text}</p>
+      </div>
+    </div>
+  );
+}
